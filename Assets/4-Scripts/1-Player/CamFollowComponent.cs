98ab29765f37c6467ac52ce5a9f26ffd09a35f7e
Quentin Ladoire <quentinladoire@gmail.com>
2021-11-08 21:39:47 +0100

    Create PlayerState and CamFollowComponent

---
+++ Assets/4-Scripts/1-Player/CamFollowComponent.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowComponent : MonoBehaviour
+{
+	[SerializeField] private Vector3 idlePosition = Vector3.zero;
+	[SerializeField] private Vector3 movePosition = new Vector3(0.0f, 0.0f, 2.0f);
+
+    private Player player = null;
+
+	private void Awake()
+	{
+		player = GetComponentInParent<Player>();
+	}
+	private void Update()
+	{
+		switch (player.State)
+		{
+			case PlayerState.Idle:
+			case PlayerState.Attack:
+				IdlePosition();
+				break;
+
+			case PlayerState.Move:
+			case PlayerState.Dodge:
+				MovePosition();
+				break;
+
+			case PlayerState.None:
+			default:
+				DefaultPosition();
+				break;
+		}
+	}
+
+	private void DefaultPosition()
+	{
+		transform.position = Vector3.zero;
+	}
+	private void IdlePosition()
+	{
+		transform.localPosition = idlePosition;
+	}
+	private void MovePosition()
+	{
+		transform.localPosition = movePosition;
+	}
+}
--- Assets/4-Scripts/1-Player/Player.cs
+++ Assets/4-Scripts/1-Player/Player.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum PlayerState
+{
+	None = -1,
+	Idle,
+	Move,
+	Dodge,
+	Attack
+}
+
 public class Player : MonoBehaviour
 {
     [SerializeField] private PlayerStats stats = null;
 
     public PlayerStats Stats => stats;
+	public PlayerState State { get; set; } = PlayerState.None;
 }
