e9da30c981d3780b2988b2390047c6f6d4411d51
Quentin Ladoire <quentinladoire@gmail.com>
2021-11-09 19:55:41 +0100

    Add callback event

--- Assets/4-Scripts/1-Player/PlayerHealthComponent.cs
+++ Assets/4-Scripts/1-Player/PlayerHealthComponent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealthComponent : MonoBehaviour
 {
@@ -8,12 +9,21 @@ public class PlayerHealthComponent : MonoBehaviour
 
 	private Player player = null;
 
+	public int Health => health;
+	public int HealthMax => player.Stats.HealthMax;
+	public int HealthInPercent => health / player.Stats.HealthMax;
+
 	public bool IsDead => health == 0;
 	public bool IsAlive => health != 0;
+
+	public UnityAction takeDamegeCallback = () => { /*Debug.Log("TakeDamageCallback");*/ };
+	public UnityAction takeHealCallback = () => { /*Debug.Log("TakeHealCallback");*/ };
 
 	private void Awake()
 	{
 		player = GetComponent<Player>();
+
+		health = player.Stats.HealthMax;
 	}
 
 	public void TakeDamege(int damage)
@@ -21,11 +31,15 @@ public class PlayerHealthComponent : MonoBehaviour
 		health -= damage;
 		if (health < 0)
 			health = 0;
+
+		takeDamegeCallback.Invoke();
 	}
 	public void TakeHeal(int heal)
 	{
 		health += heal;
 		if (health > player.Stats.HealthMax)
 			health = player.Stats.HealthMax;
+
+		takeHealCallback.Invoke();
 	}
 }
