4386259a7fa07bcd304fd9575b16a0e49ffbdca8
Quentin Ladoire <quentinladoire@gmail.com>
2021-11-09 17:39:16 +0100

    Get references of player components

--- Assets/4-Scripts/1-Player/Player.cs
+++ Assets/4-Scripts/1-Player/Player.cs
@@ -16,12 +16,27 @@ public class Player : MonoBehaviour
 
 	private Animator animator = null;
 
+	private PlayerController controller = null;
+	private PlayerDodgeComponent dodgeComponent = null;
+	private PlayerAttackComponent attackComponent = null;
+	private PlayerMovementComponent movementComponent = null;
+
     public PlayerStats Stats => stats;
 	public Animator Animator => animator;
 	public PlayerState State { get; set; } = PlayerState.None;
 
+	public PlayerController Controller => controller;
+	public PlayerDodgeComponent DodgeComponent => dodgeComponent;
+	public PlayerAttackComponent AttackComponent => attackComponent;
+	public PlayerMovementComponent MovementComponent => movementComponent;
+
 	private void Awake()
 	{
 		animator = GetComponent<Animator>();
+
+		controller = GetComponent<PlayerController>();
+		dodgeComponent = GetComponent<PlayerDodgeComponent>();
+		attackComponent = GetComponent<PlayerAttackComponent>();
+		movementComponent = GetComponent<PlayerMovementComponent>();
 	}
 }
